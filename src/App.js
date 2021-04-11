a2e38c0ac99ffd4f04b138656d2f5f2d9f7d2daa
Graham Robertson <graham@grahamcracker.dev>
2021-04-11 15:24:14 -0500

    filter by airline

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Table from './components/Table';
 import DATA from './data.js';
 
 const App = () => {
+  const [airline, setAirline] = useState('all');
+
   const formatValue = (property, value) => {
     if (property === 'airline') {
       return DATA.getAirlineById(value).name;
@@ -18,16 +20,40 @@ const App = () => {
     { name: 'Destination Airport', property: 'dest' },
   ];
 
+  const row = (airline) => (
+    <option key={airline.id} value={airline.id}>
+      {airline.name}
+    </option>
+  );
+
+  const selectAirline = (ev) => {
+    let value = ev.target.value;
+
+    if (value !== 'all') {
+      value = Number(value);
+    }
+
+    setAirline(value);
+  };
+
+  const filteredRoutes = DATA.routes.filter((route) => {
+    return route.airline === airline || airline === 'all';
+  });
+
   return (
     <div className="app">
       <header className="header">
         <h1 className="title">Airline Routes</h1>
       </header>
       <section>
+        <select onChange={selectAirline}>
+          <option value="all">Select an Airline</option>
+          {DATA.airlines.map(row)}
+        </select>
         <Table
           className="routes-table"
           columns={columns}
-          rows={DATA.routes}
+          rows={filteredRoutes}
           format={formatValue}
         />
       </section>
