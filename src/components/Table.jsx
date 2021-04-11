1733ecbfbe97b9b38d0221df267c4c0a34b58b7b
Graham Robertson <graham@grahamcracker.dev>
2021-04-11 14:58:18 -0500

    add table pagination

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Table = ({
+  className = 'table',
   columns = [{ name: 'header', property: 'value' }],
   rows = [{ id: 1, value: 'cell' }],
   format = (_, value) => value,
-  className = 'table',
+  perPage = 25,
 }) => {
-  const body = rows.map((row) => {
+  const [page, setPage] = useState(0);
+
+  const nextPage = (ev) => {
+    ev.preventDefault();
+    setPage(page + 1);
+  };
+
+  const prevPage = (ev) => {
+    ev.preventDefault();
+    setPage(page - 1);
+  };
+
+  const start = page * perPage;
+
+  const body = rows.slice(start, start + perPage).map((row) => {
     const routeInfo = columns.map((col) => {
       const routeItem = row[col.property];
       return (
@@ -20,16 +35,33 @@ const Table = ({
   });
 
   return (
-    <table className={className}>
-      <thead>
-        <tr>
-          {columns.map((col) => (
-            <th key={col.name}>{col.name}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>{body}</tbody>
-    </table>
+    <div>
+      <table className={className}>
+        <thead>
+          <tr>
+            {columns.map((col) => (
+              <th key={col.name}>{col.name}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>{body}</tbody>
+      </table>
+      <div className="pagination">
+        <p>
+          Showing {start + 1}-{start + body.length} of {rows.length} routes
+        </p>
+        <button key="previous" disabled={page === 0} onClick={prevPage}>
+          Previous Page
+        </button>
+        <button
+          key="next"
+          disabled={start + perPage >= rows.length}
+          onClick={nextPage}
+        >
+          Next Page
+        </button>
+      </div>
+    </div>
   );
 };
 
