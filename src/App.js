f8c5e182a9dfccd98e6fd6a9a6838942bf0f7164
Graham Robertson <graham@grahamcracker.dev>
2021-04-11 13:47:03 -0500

    display raw routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import DATA from "./data.js";
 
-const App = () => (
-  <div className="app">
-  <header className="header">
-    <h1 className="title">Airline Routes</h1>
-  </header>
-  <section>
-    <p>
-      Welcome to the app!
-    </p>
-  </section>
-</div>
-)
+const App = () => {
+  const row = (route) => {
+    return (
+      <tr>
+        <td>{route.airline}</td>
+        <td>{route.src}</td>
+        <td>{route.dest}</td>
+      </tr>
+    );
+  };
+
+  return (
+    <div className="app">
+      <header className="header">
+        <h1 className="title">Airline Routes</h1>
+      </header>
+      <section>
+        <table>
+          <thead>
+            <tr>
+              <th>Airline</th>
+              <th>Source Airport</th>
+              <th>Destination Airport</th>
+            </tr>
+          </thead>
+          <tbody>{DATA.routes.map(row)}</tbody>
+        </table>
+      </section>
+    </div>
+  );
+};
 
 export default App;
